b60018fd51769d975f23df643a4e6c9028397bb4
Ben Gonzales <bgonzales20001@gmail.com>
2019-05-11 20:10:52 -0500

    Edit profile

--- src/main/java/com/pioneer/configserver/pioneerconfigserver/PioneerConfigServerApplication.java
+++ src/main/java/com/pioneer/configserver/pioneerconfigserver/PioneerConfigServerApplication.java
@@ -3,9 +3,7 @@ package com.pioneer.configserver.pioneerconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class PioneerConfigServerApplication {
